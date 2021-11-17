f34caa07ecd6c2ba1237cf3fcf40aca2c9fea033
Itamar Arjuan <itamar@orbs.com>
2021-11-17 08:19:33 +0000

    Updates

--- main-cee887b4-bundle.js
+++ main-cee887b4-bundle.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	// script path function
 /******/ 	function jsonpScriptSrc(chunkId) {
-/******/ 		return __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "-" + "241e69f3" + "-bundle.js"
+/******/ 		return __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "-" + "cee887b4" + "-bundle.js"
 /******/ 	}
 /******/
 /******/ 	// The require function
@@ -346,7 +346,7 @@ eval("/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, 
 /*! exports used: version */
 /***/ (function(module) {
 
-eval("module.exports = JSON.parse(\"{\\\"a\\\":\\\"1.1.3\\\"}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWNrYWdlLmpzb24uanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./package.json\n");
+eval("module.exports = JSON.parse(\"{\\\"a\\\":\\\"1.1.4\\\"}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWNrYWdlLmpzb24uanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./package.json\n");
 
 /***/ }),
 
